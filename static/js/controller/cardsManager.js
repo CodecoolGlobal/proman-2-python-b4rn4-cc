c6b2a546e2eb445c3178ccd10be712b30ab6bc8f
barna <barna.m42@gmail.com>
2021-09-09 18:58:14 +0200

    FIX drag and drop on card create

--- static/js/controller/cardsManager.js
+++ static/js/controller/cardsManager.js
@@ -36,27 +36,28 @@ export let cardsManager = {
     createCards: async function (cardTitle, boardId) {
         let getBoardId = await boardId['boardId'];
         await dataHandler.createNewCard(cardTitle, getBoardId);
-        //TODO add when the board is open
-        await this.loadCards(getBoardId);
-        await this.columnsContainer(getBoardId)
-        await this.moveCards(getBoardId)
+
     },
 
     createCardsButton: async function () {
         const addCardButtons = await document.querySelectorAll('.board-add');
         for (let addCardButton of addCardButtons) {
-            let boardId = addCardButton.dataset['boardId']
+            let boardId = addCardButton.dataset['boardId'];
             addCardButton.addEventListener('click', async function () {
-                await cardsManager.createCards({cardTitle: 'New card'}, {boardId: boardId}, {statusId: '1'})
-                let column = this.parentElement.nextElementSibling.firstElementChild
-                let board = column.parentElement
+                await cardsManager.createCards({cardTitle: 'New card'}, {boardId: boardId}, {statusId: '1'});
+                let column = await this.parentElement.nextElementSibling.firstElementChild;
+                let board = document.querySelector(`.board-columns[data-board-id="${boardId}"]`);
                 if (column) {
-                    await clearBoard(board)
+                    await clearBoard(board);
                     await cardsManager.loadCards(boardId);
+                    await cardsManager.columnsContainer(boardId);
+                    await cardsManager.moveCards(boardId);
                 } else {
                     await cardsManager.loadCards(boardId);
+                    await cardsManager.columnsContainer(boardId);
+                    await cardsManager.moveCards(boardId);
                 }
-            })
+            });
         }
     },
     renameCards: async function (boardId) {
@@ -65,56 +66,56 @@ export let cardsManager = {
             domManager.addEventListener(`.card[data-card-id="${card.id}"`, "click", renameCardHandler);
         });
     },
-    createColumnButton: async function(){
+    createColumnButton: async function () {
         const boards = await dataHandler.getBoards();
         boards.forEach((board) => {
             domManager.addEventListener(
                 `.create-column[data-board-id="${board.id}"`,
                 "click",
                 addColumn
-            )
-        })
+            );
+        });
     },
 
     moveCards: async function (boardId) {
-        const cards = await dataHandler.getCardsByBoardId(boardId)
+        const cards = await dataHandler.getCardsByBoardId(boardId);
         for (let card of cards) {
-                    domManager.addEventListener(
-                    `.card[data-card-id="${card.id}"]`,
-                    'dragstart',
-                        dragStart
-                    )
-                    domManager.addEventListener(
-                        `.card[data-card-id="${card.id}"]`,
-                        'dragend',
-                        dragEnd
-                    )
+            domManager.addEventListener(
+                `.card[data-card-id="${card.id}"]`,
+                'dragstart',
+                dragStart
+            );
+            domManager.addEventListener(
+                `.card[data-card-id="${card.id}"]`,
+                'dragend',
+                dragEnd
+            );
         }
     },
 
     columnsContainer: async function (boardId) {
-        const columns = await dataHandler.getStatuses(boardId)
+        const columns = await dataHandler.getStatuses(boardId);
         for (let column of columns) {
             domManager.addEventListener(
                 `.board-columns[data-board-id="${boardId}"] div.board-column div.board-column-title[data-status-id="${column.id}"] div.board-column-content`,
                 'dragenter',
                 dragEnter
-            )
+            );
             domManager.addEventListener(
                 `.board-columns[data-board-id="${boardId}"] div.board-column div.board-column-title[data-status-id="${column.id}"] div.board-column-content`,
                 'dragleave',
                 dragLeave
-            )
+            );
             domManager.addEventListener(
                 `.board-columns[data-board-id="${boardId}"] div.board-column div.board-column-title[data-status-id="${column.id}"] div.board-column-content`,
                 'dragover',
                 dragOver
-            )
+            );
             domManager.addEventListener(
                 `.board-columns[data-board-id="${boardId}"] div.board-column div.board-column-title[data-status-id="${column.id}"] div.board-column-content`,
                 'drop',
                 drop
-            )
+            );
         }
     }
 };
@@ -123,35 +124,35 @@ export let cardsManager = {
 async function deleteButtonHandler(clickEvent) {
     const card = clickEvent.target.parentElement.parentElement;
     const cardId = card.dataset.cardId;
-    const board = clickEvent.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement
-    const boardId = board.dataset.boardId
-    await dataHandler.deleteCard(cardId)
-    await clearBoard(board)
-    await cardsManager.loadCards(boardId)
-    await cardsManager.columnsContainer(boardId)
-    await cardsManager.moveCards(boardId)
+    const board = clickEvent.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+    const boardId = board.dataset.boardId;
+    await dataHandler.deleteCard(cardId);
+    await clearBoard(board);
+    await cardsManager.loadCards(boardId);
+    await cardsManager.columnsContainer(boardId);
+    await cardsManager.moveCards(boardId);
 }
 
-async function clearBoard(board){
-    board.innerHTML = ""
+async function clearBoard(board) {
+    board.innerHTML = "";
 }
 
-function dragStart (clickEvent) {
-    const card = clickEvent.target
-    card.classList.add('dragging')
+function dragStart(clickEvent) {
+    const card = clickEvent.target;
+    card.classList.add('dragging');
 }
 
-function dragEnd (clickEvent) {
-    const card = clickEvent.target
-    card.classList.remove('dragging')
+function dragEnd(clickEvent) {
+    const card = clickEvent.target;
+    card.classList.remove('dragging');
 }
 
-function dragEnter (clickEvent) {
-    clickEvent.preventDefault()
+function dragEnter(clickEvent) {
+    clickEvent.preventDefault();
 }
 
-function dragLeave (clickEvent) {
-    clickEvent.preventDefault()
+function dragLeave(clickEvent) {
+    clickEvent.preventDefault();
 }
 
 function renameCardHandler(clickEvent) {
@@ -180,44 +181,44 @@ async function saveCardRenameHandler(clickEvent) {
     card.firstElementChild.firstElementChild.remove();
     card.firstElementChild.style.display = "block";
     card.firstElementChild.innerText = newCardTitle;
-    domManager.addEventListener(`.card[data-card-id="${cardId}"`, "click", renameCardHandler)
+    domManager.addEventListener(`.card[data-card-id="${cardId}"`, "click", renameCardHandler);
 }
 
-function dragOver (clickEvent) {
-    clickEvent.preventDefault()
+function dragOver(clickEvent) {
+    clickEvent.preventDefault();
 }
 
-function drop (clickEvent) {
-    clickEvent.preventDefault()
-    const column = clickEvent.currentTarget
-    const afterElement = getDragAfterElement(column, clickEvent.clientY)
-    const dragging = document.querySelector('.dragging')
+function drop(clickEvent) {
+    clickEvent.preventDefault();
+    const column = clickEvent.currentTarget;
+    const afterElement = getDragAfterElement(column, clickEvent.clientY);
+    const dragging = document.querySelector('.dragging');
     if (afterElement == null) {
-        column.appendChild(dragging)
+        column.appendChild(dragging);
     } else {
-        column.insertBefore(dragging, afterElement)
+        column.insertBefore(dragging, afterElement);
     }
 }
 
 function getDragAfterElement(column, y) {
-    const draggableElements = [...column.querySelectorAll('.card:not(.dragging)')]
+    const draggableElements = [...column.querySelectorAll('.card:not(.dragging)')];
 
     return draggableElements.reduce((closest, child) => {
-        const box = child.getBoundingClientRect()
-        const offset = y - box.top - box.height / 2
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
         if (offset < 0 && offset > closest.offset) {
-            return {offset: offset, element: child}
+            return {offset: offset, element: child};
         } else {
-            return closest
+            return closest;
         }
-    }, { offset: Number.NEGATIVE_INFINITY }).element
+    }, {offset: Number.NEGATIVE_INFINITY}).element;
 }
 
-async function addColumn(clickEvent){
-    const boardId = clickEvent.target.dataset.boardId
-    const board = clickEvent.target.parentElement.nextElementSibling
-    const statusTitle = 'New Status'
-    await dataHandler.addStatus(boardId, statusTitle)
-    await clearBoard(board)
-    await cardsManager.loadCards(boardId)
+async function addColumn(clickEvent) {
+    const boardId = clickEvent.target.dataset.boardId;
+    const board = clickEvent.target.parentElement.nextElementSibling;
+    const statusTitle = 'New Status';
+    await dataHandler.addStatus(boardId, statusTitle);
+    await clearBoard(board);
+    await cardsManager.loadCards(boardId);
 }
